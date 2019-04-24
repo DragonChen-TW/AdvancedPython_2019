6e20a9d1d8503224b39de262801f3d7140472f01
teacher144123 <teacher144123@gmail.com>
2019-04-24 22:10:18 +0800

    06 final_answer

--- 06_contests/c777.py
+++ 06_contests/c777.py
@@ -1,17 +1,5 @@
 import sys
 
-fabos = [1, 1]
-def f(n):
-    if n < len(fabos) and fabos[n]:
-        return fabos[n]
-
-    if n == 0 or n == 1:
-        return 1
-    else:
-        temp = fabos[n] = f(n - 1) + f(n - 2)
-        return temp
-
-
 n, m = sys.stdin.readline().split(' ')
 n = int(n)
 m = int(m)
