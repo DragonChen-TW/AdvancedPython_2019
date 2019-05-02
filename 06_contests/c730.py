a3091eeab10e1cf5336fd96265c29561f11ab62b
teacher144123 <teacher144123@gmail.com>
2019-05-02 16:35:41 +0800

    06 fix c730

--- 06_contests/c730.py
+++ 06_contests/c730.py
@@ -9,8 +9,12 @@ heights = [int(h) for h in heights]
 
 na = int(f[1])
 
+# normal way
 ans = 0
 for h in heights:
     if na + 30 >= h:
+        ans += 1
+print(ans)
 
-print(sum([1 for h in heights if na + 30 >= h]))
+# Python compressed way
+# print(sum([1 for h in heights if na + 30 >= h]))
